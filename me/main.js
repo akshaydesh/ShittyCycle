70ad41e47787e799012c0a6ae0fd1b74396cfedc
heartworm <shravanlal98@gmail.com>
2017-07-30 01:39:53 +1000

    falafel hsp

--- me/main.js
+++ me/main.js
@@ -2,6 +2,7 @@ class BikeRack {
     constructor (rawRack) {
         this.raw = rawRack;
         this.llp = new LatLon(rawRack.position.lat, rawRack.position.lng);
+        this.location = {lat: rawRack.position.lat, lng: rawRack.position.lng };
         this.number = rawRack.number;
         this.contract = rawRack.contract_name;
     }
@@ -80,7 +81,7 @@ Vue.component('search-screen', {
         <div class="pure-u-1">
             <input class="pure-u-1 main_input" type="text" ref="fromField" placeholder="From..." />
             <input class="pure-u-1 main_input" type="text" ref="toField" placeholder="To..."/>
-            <button class="pure-u-1" v-on:click="onNextClick()" v-bind:disabled="!fieldsCompleted">Next!</button>
+            <button class="pure-u-1 pure-button pure-button-primary" v-on:click="onNextClick()" v-bind:class="{'pure-button-disabled': !fieldsCompleted}" v-bind:disabled="!fieldsCompleted">Next!</button>
         </div>
     `,
     data: function () {
@@ -123,14 +124,14 @@ Vue.component('search-screen', {
 Vue.component('near-stops', {
     template: `
         <div class="pure-u-1" v-if="loc">
-            <p class="pure-u-1">Bike Racks Near: {{loc.name}}</p>
+            <p class="pure-u-1">The 6 nearest bike racks to: {{loc.name}}</p>
             <div class="pure-u-1" v-if="closeRacks.length">
                 <div class="pure-u-1-2" v-for="rdp in closeRacks">
-                    <p>Distance {{rdp.distance}}</p>
+                    <p>Distance {{rdp.distance.toFixed(2)}}m</p>
                     <p>{{rdp.rack.name}}</p>
                     <p>{{rdp.rack.bikes}} bikes / {{rdp.rack.spaces}} spaces</p>
                     <p>Last updated: {{rdp.rack.age.toLocaleTimeString()}}</p>
-                    <button v-on:click="chooseRack(rdp.rack)">Choose</button>
+                    <button class="pure-button" v-on:click="chooseRack(rdp.rack)">Choose</button>
                 </div>
             </div>
         </div>
@@ -145,7 +146,7 @@ Vue.component('near-stops', {
     watch: {
         loc: function(val) {
             if (this.br) {
-                this.closeRacks = this.br.nearestRacks(val.llr, 5);
+                this.closeRacks = this.br.nearestRacks(val.llr, 6);
             }
         }
     },
@@ -156,6 +157,110 @@ Vue.component('near-stops', {
         }
     }
 });
+
+Vue.component('journey-overview', {
+    template: `
+        <div class="pure-u-1">
+            <div ref="map" class="pure-u-1" style="height: 500px;"></div>
+            <div class="pure-u-1-2" v-if="cycleData">
+                <p>Cycle Distance: {{cycleData.distance.text}}</p>
+                <p>Estimated Reward: \${{(cycleData.distance.value * 0.025/1000).toFixed(2)}}</p>
+            </div>
+            <button class="pure-u-1-2"> WHERE AM I !?!?!? </button>
+        </div>
+    `,
+    props: ['to', 'from', 'fromRack', 'toRack'],
+    data: function() {
+        return {
+            map: null,  
+            cycleData: null,
+        }
+    },
+    methods: {
+        mapsReady: function() {
+            console.log("mapsreead");
+            this.mapBounds = new google.maps.LatLngBounds();
+            this.directionsService = new google.maps.DirectionsService();
+            this.directionRenderers = [
+                new google.maps.DirectionsRenderer({
+                    map: this.map,
+                    preserveViewport: true,
+                    polylineOptions: {
+                        strokeColor: 'red'
+                    },
+    //                panel: document.getElementById('panel').appendChild(document.createElement('li'))
+                }),
+                new google.maps.DirectionsRenderer({
+                    map: this.map,
+                    preserveViewport: true,
+                    polylineOptions: {
+                        strokeColor: 'blue'
+                    },
+    //                panel: document.getElementById('panel').appendChild(document.createElement('li'))
+                }),
+                new google.maps.DirectionsRenderer({
+                    map: this.map,
+                    preserveViewport: true,
+                    polylineOptions: {
+                        strokeColor: 'yellow'
+                    },
+    //                panel: document.getElementById('panel').appendChild(document.createElement('li'))
+                })
+            ];
+        },
+        drawDirections: function() {
+            let to = { placeId: this.to.place_id };
+            let from = { placeId: this.from.place_id };
+            let fromRack = this.fromRack.location;
+            let toRack = this.toRack.location;
+            if (to && from && fromRack && toRack) {
+                let waypoints = [{
+                    origin: from,
+                    destination: fromRack,
+                    travelMode: google.maps.TravelMode.WALKING
+                }, {
+                    origin: fromRack,
+                    destination: toRack,
+                    travelMode: google.maps.TravelMode.BICYCLING
+                }, {
+                    origin: toRack,
+                    destination: to,
+                    travelMode: google.maps.TravelMode.WALKING
+                }];
+                for (let x = 0; x < 3; x++) {
+                    this.directionsService.route(waypoints[x], (result, status) => {
+                        if (status == google.maps.DirectionsStatus.OK) {
+                            this.directionRenderers[x].setDirections(result);
+                            this.map.fitBounds(this.mapBounds.union(result.routes[0].bounds));
+                            
+                            if (x == 1) {
+                                this.cycleData = {
+                                    distance: result.routes[0].legs[0].distance
+                                }
+                            }
+                            
+                        } else {
+                            alert("No Directions found for a leg of the journey!");
+                        }
+                    });
+                }
+            }
+        }
+    },
+    mounted: function() {
+        this.map = new google.maps.Map(this.$refs.map, {
+            zoom: 15,
+            center: {
+                lat: -27.470004,
+                lng: 153.025007
+            }
+        });
+        this.mapsReady();
+        this.drawDirections();
+    }    
+});
+
+
 
 var app = new Vue({
     el: '#app',
@@ -179,7 +284,13 @@ var app = new Vue({
         onChosenRack: function() {
             this.fromRack = this.$refs.fromStops.chosenRack;
             this.toRack = this.$refs.toStops.chosenRack;
-            this.screen++;
+            this.screen++;            
+        },
+        
+    },
+    computed: {
+        allLocs: function() {
+            return {from: this.from, fromRack: this.fromRack, toRack: this.toRack, to: this.to};
         }
-    }
+    } 
 });
