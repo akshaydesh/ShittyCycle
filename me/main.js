95680ca47695214990ff8128d965a29c8e686815
heartworm <shravanlal98@gmail.com>
2017-07-29 14:55:23 +1000

    dawrrrrr

---
+++ me/main.js
@@ -0,0 +1,82 @@
+class BikeRack {
+    constructor (rawRack) {
+        this.raw = rawRack;
+        this.llp = new LatLon(rawRack.position.lat, rawRack.position.lng);
+        this.number = rawRack.number;
+        this.contract = rawRack.contract_name;
+    }
+    
+    update() {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", `https://api.jcdecaux.com/vls/v1/stations/${this.number}?contract=${this.contract}&apiKey=2b124043623ae712a8c465cf27e1847d8380e759`);
+        xhr.onload = () => this.raw = xhr.response;
+        xhr.send();
+    }
+    
+    get isOpen() {
+       return this.raw.status == "OPEN";
+    }
+    
+    get capacity() {
+        return this.raw.bike_stands;
+    }
+    
+    get bikes() {
+        return this.raw.available_bikes;
+    }
+    
+    get spaces() {
+        return this.raw.available_bike_stands
+    }
+    
+    get name() {
+        return this.raw.name;
+    }
+    
+    get address() {
+        return this.raw.address;
+    }   
+    
+    get age() {
+        return new Date(this.raw.last_update);
+    }
+};
+class BikeRacks {
+    constructor() {
+        console.log("hello");
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://api.jcdecaux.com/vls/v1/stations?apiKey=2b124043623ae712a8c465cf27e1847d8380e759&contract=Brisbane");
+        xhr.responseType = "json";
+        xhr.onload = () => this.onLoadRacks(xhr.response);
+        
+        xhr.send();
+        
+        this.ready = false;
+    }
+    
+    onLoadRacks(rawRacks) {
+        this.racks = [];
+        for (let rack of rawRacks) {
+            this.racks.push(new BikeRack(rack));
+        }
+        this.ready = true;
+    }
+        
+    nearestRacks(llp, radius) {
+        var output = [];
+        for (let rack of this.racks) {
+            output.push({rack: rack, distance: rack.llp.distanceTo(llp)});
+        }
+        output.sort((a, b) => {
+           return a.distance - b.distance; 
+        });
+        return output;
+    }
+}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        bikeRacks: new BikeRacks()
+    }
+});
